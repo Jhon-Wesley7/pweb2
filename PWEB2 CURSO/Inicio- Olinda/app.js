6ac665b92807d336ec90cad88e3b588f12415b3a
Jhon Wesley <128758538+Jhon-Wesley7@users.noreply.github.com>
2023-10-16 17:29:37 -0300

    Add files via upload

---
+++ PWEB2 CURSO/Inicio- Olinda/app.js
@@ -0,0 +1,14 @@
+//Testes sendo feito em produção 
+alert(messagem); // MOstra a variavel 
+
+let user = 'Jhon';
+let age = 25;
+let cpfm = '087.919.143-02';
+const myBirthday = '06.02.2003';
+let aviso = 'Hello Jhon, undersntand?';
+
+let num1 = 4;
+let num2 = 5;
+a = num1 + num2;
+
+console.log(a + num1);
