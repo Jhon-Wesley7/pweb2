55b6fb2d17fc6e829720c693f734076181539a11
Jhon Wesley <128758538+Jhon-Wesley7@users.noreply.github.com>
2023-12-05 19:57:55 -0300

    Add files via upload

--- PWEB2 CURSO/Hoje05122023/js/app.js
+++ PWEB2 CURSO/Hoje05122023/js/app.js
@@ -6,3 +6,35 @@ const btnAnterior = divImagemPrincipal.querySelector(".anterior")
 const todasImagens = document.querySelectorAll ("#imagens img")
 
 let idImagemAtiva = 0
+
+const proximaImagem = function () {
+    idImagemAtiva++
+    if(idImagemAtiva >= todasImagens.length) 
+        idImagemAtiva = 0
+    selecionarImagem()
+}
+
+const voltarImagem = function () {
+    idImagemAtiva--
+    if(idImagemAtiva < 0 ) 
+        idImagemAtiva = todasImagens.lenght - 1
+    selecionarImagem()
+}
+
+const selecionarImagem = function() {
+    imagemPrincipal.src = todasImagens[idImagemAtiva].src
+    todasImagens.forEach(function(imagem){
+        imagem.classList = "";
+    })
+    todasImagens[idImagemAtiva].classList.add("")
+}
+
+btnProximo.addEventListener("click", proximaImagem)
+btnAnterior.addEventListener("click", voltarImagem)
+
+todasImagens.forEach( function(imagem, numeroImage){
+    imagem.addEventListener("click", function(){
+        idImagemAtiva = numeroImage
+        selecionarImagem()
+    })
+})
